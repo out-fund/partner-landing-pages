171d3087efee9e61b87092e4479b7032c3ffd031
Stjepan Grgic <grgic.stjepan@gmail.com>
2022-03-14 19:32:09 +0000

    Visual improvements

--- src/components/LandingPageLayout.js
+++ src/components/LandingPageLayout.js
@@ -1,25 +1,65 @@
 import React from "react"
 import { graphql } from "gatsby"
+import styled from "styled-components"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const LandingPageLayout = ({ data: { landingPagesYaml } }) => {
   console.log(landingPagesYaml)
-  const { title, description, image } = landingPagesYaml
+  const { title, description, image, buttonColor } = landingPagesYaml
   return (
-    <main>
-      <h1>{title}</h1>
-      <p>{description}</p>
+    <Main>
+      <header>
+        <div className="text">
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </div>
+        <div className="image">
+          <GatsbyImage image={getImage(image)} alt="test" />
+        </div>
+      </header>
+
       <div>
-        <GatsbyImage image={getImage(image)} alt="test" />
+        <Button
+          href="https://calendly.com/alexis-211/growth-capital-consultation-with-outfund?month=2022-03"
+          style={{ backgroundColor: `${buttonColor}` }}
+        >
+          Get funded
+        </Button>
       </div>
-      <div>
-        <button>Get funded</button>
-      </div>
-    </main>
+    </Main>
   )
 }
 
 export default LandingPageLayout
+
+const Main = styled.main`
+  padding-top: 100px;
+  font-family: sans-serif;
+  max-width: 800px;
+  margin: 0 auto;
+  .text {
+    display: flex;
+    flex-direction: column;
+  }
+  header {
+    display: flex;
+    gap: 80px;
+  }
+  .image {
+    min-width: 200px;
+  }
+`
+
+const Button = styled.a`
+  text-decoration: none;
+  color: #000;
+  display: inline-block;
+  padding: 10px 20px;
+  margin: 0;
+  border: none;
+  font-weight: bold;
+  border-radius: 100px;
+`
 
 export const query = graphql`
   query LandingPageQuery($pageId: String) {
@@ -27,6 +67,7 @@ export const query = graphql`
       slug
       title
       description
+      buttonColor
       image {
         childImageSharp {
           gatsbyImageData
